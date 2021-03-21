c69ac0cb0f65a5acc2c56d1fb3fe3ea270dabc25
Andrei Dobrinski <andreiidobrinski@gmail.com>
2021-03-21 09:52:40 -0400

    add testing libraries

---
+++ __mocks__/gatsby.js
@@ -0,0 +1,36 @@
+import config from '../gatsby-config';
+const React = require("react")
+const gatsby = jest.requireActual("gatsby")
+
+module.exports = {
+  ...gatsby,
+  graphql: jest.fn(),
+  Link: jest.fn().mockImplementation(
+    // these props are invalid for an `a` tag
+    ({
+      activeClassName,
+      activeStyle,
+      getProps,
+      innerRef,
+      partiallyActive,
+      ref,
+      replace,
+      to,
+      ...rest
+    }) =>
+      React.createElement("a", {
+        ...rest,
+        href: to,
+      })
+  ),
+  StaticQuery: jest.fn().mockImplementationOnce(({ render }) =>
+    render({
+      data: {
+        site: {
+          siteMetadata: config.siteMetadata
+        },
+      }
+    })
+  ),
+  useStaticQuery: jest.fn(),
+}
